5c9a07ed2a4492d1417fb10b858f477deaa617a6
FERNANDO DE LA IGLESIA MEDINA <fernando.delaiglesia@gmail.com>
2017-04-10 14:38:07 +0200

    Modify server to get the image from StashBox. Add request as a dependncy

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var formidable = require('formidable'),
     http = require('http'),
     util = require('util'),
-    fs = require('fs');
+    fs = require('fs'), 
+    request = require('request'),
+    imagen = 'http://192.168.99.100/jupiter.jpg';
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+
+
  
 http.createServer(function(req, res) {
   /* Process the form uploads */
@@ -19,19 +33,20 @@ http.createServer(function(req, res) {
   //switch(req.url) {
   //	case '/logo.jpg':
   if (req.url == '/logo.jpg') { 
-      var img = fs.readFileSync('./logo.jpg');
+      
+     var img = fs.readFileSync('./logo.jpg');
         res.writeHead(200, {'Content-Type': 'image/jpeg'});
         res.end(img,'binary');
   //	break;
   return;
   
   }
-   
-
-
  
   /* Display the file upload form. */
   res.writeHead(200, {'content-type': 'text/html'});
+  download(imagen, './logo.jpg', function(){
+  	console.log('done');
+  });
   res.end(
     '<html><head><title>Hello Noder!</title></head><body background="/logo.jpg">'+
     '<h1>Welcome Noder, who are you?</h1>'+
